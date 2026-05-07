3ff6bc279f2e3b97ef97140379fd36bb31a9ccdf
Arjun Jalanela <arjunjalanela@gmail.com>
2026-05-07 13:33:29 +0530

    dto

---
+++ src/main/java/com/project/hotel/repository/BookingRepository.java
@@ -0,0 +1,11 @@
+package com.project.hotel.repository;
+
+import com.project.hotel.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByUserId(Long userId);
+}
---
+++ src/main/java/com/project/hotel/repository/HotelRepository.java
@@ -0,0 +1,11 @@
+package com.project.hotel.repository;
+
+import com.project.hotel.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+
+    List<Hotel> findByLocationContainingIgnoreCase(String location);
+}
